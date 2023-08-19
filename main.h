dd6d112ea263a3d76a21d9914cc6aaa049e13d47
hassantarrek <htarek168@gmail.com>
2023-08-19 09:33:22 -0700

    UPDATE main.h

--- main.h
+++ main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 /**
  * header file
  * function _printf
@@ -6,3 +13,5 @@
 int _printf(const char *format, ...);
 char print_string (char *src);
 char print_char (char c);
+
+#endif
