d3efa28f73603cc79df87751a34694a2d8737bea
hassantarrek <htarek168@gmail.com>
2023-08-20 16:49:24 -0700

    UPDATING string function

--- print_string.c
+++ print_string.c
@@ -1,20 +1,30 @@
 #include "main.h"
 
 /**
-* print_string - print a string
-* @src : source to string
-* Return : no return
+* print_string - print a string 'logicly'
+* @val: arg
+* Return: length of the strong string
 */
 
-int print_string(char *src)
+int print_string(va_list val)
 {
-	int i;
+	char *u;
+	int x, len;
 
-	i = 0;
-
-	while (src[i] != '\0')
+	u = va_arg(val, char *);
+	if (u == NULL)
 	{
-		write(1, &src[i], 1);
-		i++;
+		u = "(null)";
+		len = _strlen(u);
+		for (x = 0; x < len; x++)
+			_putchar(u[x]);
+		return (len);
+	}
+	else
+	{
+		len = _strlen(u);
+		for (x = 0; x < len; x++)
+			_putchar(u[x]);
+		return (len);
 	}
 }
