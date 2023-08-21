be6c6ade2c00ae831d0dc77dca51c5d85f9ae5ef
hassantarrek <htarek168@gmail.com>
2023-08-21 00:43:48 -0700

    adding function

---
+++ print_int.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * printf_int - prints integer
+ * @args: arg
+ * Return: number of printed things
+ */
+
+int printf_int(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit, exp = 1;
+	int  x = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		x++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			x++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (x);
+}
