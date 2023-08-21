eb58ffdc60fa13952d7e1a069c6004e34ad72e73
hassantarrek <htarek168@gmail.com>
2023-08-21 01:10:39 -0700

    udapte pr_percent

--- print_percent.c
+++ print_percent.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_percent - prints %
+ * pr_percent - prints %
  * Return: 1
  */
 
-int print_percent(void)
+int pr_percent(void)
 {
 	_putchar('%');
 	return (1);
