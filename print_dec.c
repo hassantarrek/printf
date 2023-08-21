cfcf0df9254623ce6b6ae481fc334de7a501f3bf
hassantarrek <htarek168@gmail.com>
2023-08-21 00:46:23 -0700

    adding function

---
+++ print_dec.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * printf_dec - prints decimal
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+
+int printf_dec(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit;
+	int  x = 1;
+	int exp = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		x++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			x++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (x);
+}
