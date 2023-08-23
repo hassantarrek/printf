2d76d6d605126671a1edd85bd06796295cdb0f1e
shaimaa <shaimafikry@yahoo.com>
2023-08-23 04:39:58 -0700

    edit filrs

--- pr_binary.c
+++ pr_binary.c
@@ -4,7 +4,7 @@
  * @args: takes variadic func argument
  * Return: count of printed digits
  */
- 
+
 int binary(va_list args)
 {
 	unsigned int n = va_arg(args, unsigned int);
@@ -12,16 +12,17 @@ int binary(va_list args)
 
 	count = 0;
 	m = 0;
-	while(n != 0)
+	while (n != 0)
 	{
-		res[m]= n % 2;
+		res[m] = n % 2;
 		n /= 2;
 		m++;
 	}
 	for (i = m - 1; i >= 0; i--)
-		{
+	{
 		_putchar(res[i] + '0');
 		count++;
-		}
-	 return (count);
+	}
+	return (count);
 }
+
