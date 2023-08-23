6540449e71a82156b9b37aa6c16af5eecfb22c3b
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-23 14:26:28 +0300

    Update pr_oct.c

--- pr_oct.c
+++ pr_oct.c
@@ -4,7 +4,7 @@
  * @args: takes variadic func argument
  * Return: count of printed digits
  */
- 
+
 int pr_oct(va_list args)
 {
 	int arr[10];
@@ -13,13 +13,13 @@ int pr_oct(va_list args)
 
 	i = 0;
 	count = 0;
-	while(n != 0)
+	while (n != 0)
 	{
 		arr[i] = (n % 8);
 		n = n / 8;
 		i++;
 	}
-	while ( i >= 0)
+	while (i >= 0)
 	{
 		if (arr[i] >= 0 && arr[i] <= 9)
 		{
