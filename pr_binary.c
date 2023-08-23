a9b53189de5b748bc76260b75a1384d043fdeae6
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-23 14:16:58 +0300

    Update pr_binary.c

--- pr_binary.c
+++ pr_binary.c
@@ -1,23 +1,27 @@
-#include<stdio.h>
-void binary(int x){
-int res[32],m=0,i;
-while(x!=0){
-res[m]=x%2;
-x/=2;
-m++;
+#include <main.h>
+/**
+ * binary - print binary number
+ * @args: takes variadic func argument
+ * Return: count of printed digits
+ */
+ 
+int binary (va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+	int res[32], m, i;
+
+	int count = 0;
+	int m = 0;
+	while(n != 0)
+	{
+		res[m]= n % 2;
+		n /= 2;
+		m++;
+	}
+	for (i = m - 1; i >= 0; i--)
+		{
+		_put_char(res[i] + '0');
+		count++;
+		}
+	 return (count);
 }
-for (i = m-1; i >=0; i--)
-  printf("%d",res[i]);
-printf("\n") ;
-
-}
-int main (){
-int number,i;
-printf("enter a number \n" );
-scanf("%d",&number );
-for ( i = 1; i <= number; i++)
-binary(i);
-
-  return 0;
- }
-
