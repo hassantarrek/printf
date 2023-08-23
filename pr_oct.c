53f1ce9aa06deaf64b22f6075a73ef7d07b369d4
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-23 14:04:58 +0300

    Update pr_oct.c

--- pr_oct.c
+++ pr_oct.c
@@ -7,26 +7,26 @@
  
 int pr_oct (va_list args)
 {
- int arr[10];
- int count, i;
- unsigned int n = va_arg(args, unsigned int);
- i = 0;
- count = 0;
+	int arr[10];
+	int count, i;
+	unsigned int n = va_arg(args, unsigned int);
 
- 	while(n != 0)
- 		{
-			arr[i] = (n % 8);
-			n = n / 8;
-			i++;
+	i = 0;
+	count = 0;
+	while(n != 0)
+	{
+		arr[i] = (n % 8);
+		n = n / 8;
+		i++;
+	}
+	while ( i >= 0)
+	{
+		if (arr[i] >= 0 && arr[i] <= 9)
+		{
+			_putchar(arr[i] + '0');
+			count++;
 		}
- 	while ( i >= 0)
-         	{
-        	 if (arr[i] >= 0 && arr[i] <= 9)
-            	{
-             		_putchar(arr[i] + '0');
-            		 count++;
-	    	}
-             	i--;
-         	}
-	 return (count);
+		i--;
+	}
+	return (count);
 }
